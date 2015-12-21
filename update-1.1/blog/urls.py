778a01187723c69104bb7086c6cf2b10b9bcf22b
agus makmun <summon.agus@gmail.com>
2015-12-22 01:08:18 +0700

    Adding Url for outputing post with json format

--- update-1.1/blog/urls.py
+++ update-1.1/blog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     url(r'^$', views.BlogIndex.as_view(), name="index"),
     url(r'^blog/(?P<slug>\S+)/$', views.BlogDetail.as_view(), name="entry_detail"),
     url(r'^page/(?P<slug>\S+)/$', views.PageDetail.as_view(), name="page_detail"),
+
+    #outputing post with json format
+    url(r'^json-posts/(?P<pk>\S+)/$', json_format.json_default_posts, name="page_json_default_post"),
 ]
