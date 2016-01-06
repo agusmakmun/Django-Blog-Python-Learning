c70e810bcf23fd15a1bc130c8ce0a399d9d74e49
agus makmun <summon.agus@gmail.com>
2016-01-07 00:09:22 +0700

    update country code and domain name for filtering

--- update-1.1/blog/views.py
+++ update-1.1/blog/views.py
@@ -33,21 +33,35 @@ class BlogDetail(generic.DetailView):
 
     def tracking_hit_post(self):
         entry = self.model.objects.get(pk=self.object.id)
-        filter_dns = ['googlebot', 'yahoo', 'bing','solidseodedicated'] #this such as google boot, or other boot.
-        
+        filter_dns = ['ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'an', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 
+        'az', 'ba', 'bb', 'bd', 'be', 'bf', 'bg', 'bh', 'bi', 'bj', 'bm', 'bn', 'bo', 'br', 'bs', 'bt', 'bv', 
+        'bw', 'by', 'bz', 'ca', 'cc', 'cd', 'cf', 'cg', 'ch', 'ci', 'ck', 'cl', 'cm', 'cn', 'co', 'cr', 'cu', 
+        'cv', 'cx', 'cy', 'cz', 'de', 'dj', 'dk', 'dm', 'do', 'dz', 'ec', 'ee', 'eg', 'eh', 'er', 'es', 'et', 
+        'fi', 'fj', 'fk', 'fm', 'fo', 'fr', 'fx', 'ga', 'gd', 'ge', 'gf', 'gh', 'gi', 'gl', 'gm', 'gn', 'gp', 
+        'gq', 'gr', 'gs', 'gt', 'gu', 'gw', 'gy', 'hk', 'hm', 'hn', 'hr', 'ht', 'hu', 'id', 'ie', 'il', 'in', 
+        'io', 'iq', 'ir', 'is', 'it', 'jm', 'jo', 'jp', 'ke', 'kg', 'kh', 'ki', 'km', 'kn', 'kp', 'kr', 'kw', 
+        'ky', 'kz', 'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma', 'mc', 'md', 'mg', 
+        'mh', 'mk', 'ml', 'mm', 'mn', 'mo', 'mp', 'mq', 'mr', 'ms', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz', 
+        'na', 'nc', 'ne', 'nf', 'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nu', 'nz', 'om', 'pa', 'pe', 'pf', 'pg', 
+        'ph', 'pk', 'pl', 'pm', 'pn', 'pr', 'pt', 'pw', 'py', 'qa', 're', 'ro', 'ru', 'rw', 'sa', 'sb', 'sc', 
+        'sd', 'se', 'sg', 'sh', 'si', 'sj', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'st', 'sv', 'sy', 'sz', 'tc', 
+        'td', 'tf', 'tg', 'th', 'tj', 'tk', 'tm', 'tn', 'to', 'tp', 'tr', 'tt', 'tv', 'tw', 'tz', 'ua', 'ug', 
+        'uk', 'um', 'us', 'us', 'uy', 'uz', 'va', 'vc', 've', 'vg', 'vi', 'vn', 'vu', 'wf', 'ws', 'ye', 'yt', 
+        'yu', 'za', 'zm', 'zr', 'zw', 'com', 'net', 'org', 'me', 'biz', 'asia', 'ninja', 'club', 'online', 'website', 'site']
+
         try:
         	models.Entry_Views.objects.get(entry=entry, ip=self.get_client_ip(), session=self.request.session.session_key)
         except ObjectDoesNotExist:
         	import socket
         	dns = socket.getfqdn(str(self.get_client_ip()))
-        	if str(dns).split('.') not in filter_dns:
+        	if str(dns).split('.')[-1] not in filter_dns:
                 	view = models.Entry_Views(entry=entry, 
                 			  ip=self.request.META['REMOTE_ADDR'],
                 			  created=datetime.datetime.now(),
                 			  session=self.request.session.session_key)
                 	view.save()
                 else: pass
-	    return models.Entry_Views.objects.filter(entry=entry).count()
+	return models.Entry_Views.objects.filter(entry=entry).count()
 	
     def get_context_data(self, **kwargs):
     	context_data = super(BlogDetail, self).get_context_data(**kwargs)
