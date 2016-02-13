2e7f50dd8286d9e79325ef5ae16e27b07a9c384a
agus makmun <summon.agus@gmail.com>
2016-02-13 21:27:28 +0700

    Modified RSS
    Docs: https://python.web.id/blog/building-an-rss-feed-for-your-django-content/

--- update-1.1/blog/feed.py
+++ update-1.1/blog/feed.py
@@ -1,10 +1,35 @@
 from django.contrib.syndication.views import Feed
+from django.utils.feedgenerator import Rss201rev2Feed
+from django.core.urlresolvers import reverse
 from .models import Entry
 
+class CorrectMimeTypeFeed(Rss201rev2Feed):
+	mime_type = 'application/xml'
+    
 class LatestPosts(Feed):
-    title = "Q Blog"
-    link = "/feed/"
-    description = "Latest Posts of Q"
+	feed_type = CorrectMimeTypeFeed
+	
+	title = "Feed Blog Posts"
+	link = "/feed/"
+	description = "Latest Feed Blog Posts"
 
-    def items(self):
-        return Entry.objects.published()[:5]
+	def author_name(self):
+		return "Summon Agus"
+        
+	def items(self):
+		return Entry.objects.published()[:10]
+
+	def item_title(self, item):
+		return item.title
+
+	def item_description(self, item):
+		return item.body
+
+	def item_author_name(self, item):
+		return item.author
+        
+	def item_link(self, item):
+		return reverse('entry_detail', args=[item.slug])
+	
+	def item_pubdate(self, item):
+		return item.modified
