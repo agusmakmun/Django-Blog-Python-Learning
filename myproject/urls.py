75076ec8e19250d65e494b66284d539bda0abed7
agusmakmun <summon.agus@gmail.com>
2016-05-02 00:06:34 +0700

    Fix locate of handlers

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,18 +7,18 @@ from django.contrib.sitemaps import GenericSitemap
 from django.contrib.sitemaps.views import sitemap
 from blog.models import Entry
 
+from django.conf.urls import (
+    handler400, handler403, handler404, handler500
+)
+handler400 = 'views.handler400'
+handler403 = 'views.handler403'
+handler404 = 'views.handler404'
+handler500 = 'views.handler500'
+
 info_dict = {
     'queryset': Entry.objects.all(),
     'date_field': 'modified',
 }
-
-from django.conf.urls import (
-    handler400, handler403, handler404, handler500
-)
-handler400 = 'blog.views.handler400'
-handler403 = 'blog.views.handler403'
-handler404 = 'blog.views.handler404'
-handler500 = 'blog.views.handler500'
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
